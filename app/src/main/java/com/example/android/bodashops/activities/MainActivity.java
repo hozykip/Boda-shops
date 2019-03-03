42abbcc69ecffa3b79c818214a596e1bf9c1d9e8
hozy kip <hozykip59@gmail.com>
2019-03-03 21:00:18 +0300

    HomeActivity orders

--- app/src/main/java/com/example/android/bodashops/activities/MainActivity.java
+++ app/src/main/java/com/example/android/bodashops/activities/MainActivity.java
@@ -11,19 +11,34 @@ import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.android.bodashops.fragments.AllOrdersFragment;
 import com.example.android.bodashops.fragments.NotificationsFragment;
 import com.example.android.bodashops.R;
+import com.example.android.bodashops.fragments.PendingOrdersFragment;
+import com.example.android.bodashops.fragments.TodaysOrdersFragment;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements
         NotificationsFragment.OnFragmentInteractionListener {
 
 
     private DrawerLayout mDrawerLayout;
+
+    private ViewPager pager;
+    //private PagerSlidingTabStrip tabs;
+    TabLayout tabs;
 
     private Intent myIntent;
 
@@ -44,6 +59,15 @@ public class MainActivity extends AppCompatActivity implements
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
 
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new MainActivity.ViewPagerAdapter(getSupportFragmentManager()));
+        setupViewPager(pager);
+
+        // Bind the tabs to the ViewPager
+        tabs = (TabLayout) findViewById(R.id.tabsHome);
+
+        tabs.setupWithViewPager(pager);
+
         //handle navigation click events
         mNavigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
@@ -62,11 +86,11 @@ public class MainActivity extends AppCompatActivity implements
                                 startActivity(myIntent);
                                 break;
                             case R.id.ordersmenu:
-                                myIntent = new Intent(MainActivity.this, OrdersActivity.class);
+                                myIntent = new Intent(MainActivity.this, ItemsActivity.class);
                                 startActivity(myIntent);
                                 break;
                             case R.id.notificationsmenu:
-                                myIntent = new Intent(MainActivity.this, OrdersActivity.class);
+                                myIntent = new Intent(MainActivity.this, ItemsActivity.class);
                                 startActivity(myIntent);
                                 break;
                         }
@@ -81,6 +105,13 @@ public class MainActivity extends AppCompatActivity implements
         );
     }
 
+    private void setupViewPager(ViewPager viewPager) {
+        MainActivity.ViewPagerAdapter adapter = new MainActivity.ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+    }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
@@ -89,6 +120,45 @@ public class MainActivity extends AppCompatActivity implements
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        // tab titles
+        private String[] tabTitles = new String[]{"New", "Today", "All"};
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        // overriding getPageTitle()
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new PendingOrdersFragment();
+                case 1:
+                    return new TodaysOrdersFragment();
+                case 2:
+                    return new AllOrdersFragment();
+                default:
+                    return null; // shouldn't happen
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return tabTitles.length;
         }
     }
 
